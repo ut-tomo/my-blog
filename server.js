cf8d18dc3d693f71705620a3f3f93aaa6649c819
1031Tomoyuki <unno.tomoyuki01@gmail.com>
2025-06-17 15:34:22 +0900

    organized

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// publicフォルダの静的ファイルを配信
 app.use(express.static('public'));
 
-// "/" にアクセスが来たときは index.html を返す
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
