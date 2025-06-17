8d7a0eb192208fe9505f95a13e3d5285f8c702f5
1031Tomoyuki <unno.tomoyuki01@gmail.com>
2025-06-17 15:14:23 +0900

    server.js changed

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// publicフォルダの静的ファイルを配信
 app.use(express.static('public'));
+
+// "/" にアクセスが来たときは index.html を返す
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
